45242e9158cd69883ab483225bc8f1d47dc92ea9
Simon Howey <simon.howey@gmail.com>
2014-04-30 17:48:38 -0700

    added basic user model including secure password

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,40 +1,63 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit root_path
-      expect(page).to have_content('Sample App')
-      expect(page).not_to have_title(' | Home')
-    end
+  subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading)}
+    it { should have_title(full_title(page_title))}
+  end
+
+  describe "Home page" do
+    before { visit root_path }
+
+    let(:heading) {'Sample App'}
+    let(:page_title) {''}
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title '| Home'}
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-      expect(page).to have_title('Help')
-    end
+    before {visit help_path}
+
+    let(:heading) {'Help'}
+    let(:page_title) {'Help'}
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
-    it "Should have the content 'About Us'" do
-      visit about_path
-      expect(page).to have_content('About Us')
-      expect(page).to have_title('About Us')
-    end
+    before { visit about_path }
+
+    let(:heading) {'About'}
+    let(:page_title) {'About'}
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
-    it "Should have the title 'Contact'" do
-      visit contact_path
-      expect(page).to have_title('Contact')
-    end
+    before { visit contact_path }
 
-    it "Should have the content 'Contact'" do
-      visit contact_path
-      expect(page).to have_content('Contact')
-    end
+    it {should have_selector('h1', text: 'Contact') }
+    it {should have_title(full_title('Contact')) }
+  end
+
+  #LINKS
+
+  it "should have the right links on the layoutpoo" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign Up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
   end
 end
