8a0edb1c2746adca467e6c98d8d4f22c8b38e15d
Simon Howey <simon.howey@gmail.com>
2014-04-30 22:28:50 -0700

    adding a few little changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 
   validates :name,
             presence: true,
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   validates :password,
             length: { minimum: 6 }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email,
             presence: true,
             format: { with: VALID_EMAIL_REGEX },
